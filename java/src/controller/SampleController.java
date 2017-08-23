d6afff6dc24f726d7a0a383418ffa93ceb2d1792
starmoon1994@163.com <starmoon1994@163.com>
2017-08-23 12:01:23 +0800

    第一次构建应用

---
+++ java/src/controller/SampleController.java
@@ -0,0 +1,26 @@
+package src.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 基础启动类
+ * Created by huxingyue on 2017/8/23.
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
